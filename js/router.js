344c2a83f6900e3b6f2e22b7c74d3a1f44af6bc2
Katriin <katriin.arme@gmail.com>
2024-10-28 10:00:09 +0200

    Lisan router faili koos sisuga

---
+++ js/router.js
@@ -0,0 +1,29 @@
+import { loadCategoryView } from './loadCategoryView.js';
+import { loadProductView } from './productView.js';
+import { loadCartView } from './cartView.js';
+
+// Funktsioon, mis vastutab vaadete vahel liikumise eest
+
+export const navigate = (view, parem) => {
+    const views = {
+        'category': () => loadCategoryView(parem || 'all'),
+        'product': () => loadProductView(parem), //Näita toote detailvaadet
+        'cart': () => loadCartView(), // Näita ostukorvi vaadet
+    };
+
+    //Vali ja käivita sobiv vaade
+    if (views[view]) {
+        views[view]();
+
+        //Muuda URL-i ilma lehte uuesti laadimata
+        const newUrl = view === 'category' ? '/' : `/${view}/${parem || ''}`;
+        window.history.pushState({}, '', newUrl);
+    }
+};
+
+// Sündmuse kuulaja, kui kasutaja vajutab "tagasi" või "edasi" nuppu brauseris
+window.addEventListener('popstate', () => {
+    const path = window.location.pathname;
+    const [_, view, param] = path.split('/');
+    navigate(view || 'category', param);
+});
