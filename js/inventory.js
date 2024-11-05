1148444cb8930d7bc05edaff1f6185f6f61c3d7e
Katriin <katriin.arme@gmail.com>
2024-11-05 21:54:01 +0200

    Välja kommenteeritu kustutamine

--- js/inventory.js
+++ js/inventory.js
@@ -16,7 +16,7 @@ export class Inventory {
 
     getAllProducts() {
         return this.products;
-}
+    }
 
     getAvailableProducts() {
         return this.products.filter(product => product.quantity > 0);
@@ -26,29 +26,3 @@ export class Inventory {
         return this.products.find(product => product.name === name);
     }
 }
-
-
-// addProduct (product, quantity) {
-//     this.stock[product.name] = quantity;
-// }
-
-// checkStock(productName) {
-//     return this.stock[productName] || 0;
-// }
-
-// reduceStock(productName, quantity) {
-//     if (this.stock[productName] >= quantity) {
-//         this.stock[productName] -= quantity;
-//         return true;
-//         }
-//         return false;
-// }
-
-//     increaseStock(productName, quantity) {
-//         this.stock[productName] += quantity;
-//     }
-
-// // Add product to the shopping cart
-// window.addToCart = function (product) {
-//     const stock = inventory
-// }
